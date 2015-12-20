912176cf8268951953d693b980cb46ce750e60cb
Jared Radin <jared.radin@gmail.com>
2015-12-19 20:18:03 -0800

    more layout, some images are horizontal

--- js/index.js
+++ js/index.js
@@ -6,19 +6,19 @@ require('../scss/index.scss');
 console.log('this is javascript')
 
 function render() {
-  let src = 'https://placekitten.com/1000/700'
-
   let i = 20;
 
   while (i--) {
+    let src = i % 3 === 0 ? 'https://placekitten.com/700/1000' : 'https://placekitten.com/1000/700'
+
     let showcase = document.getElementById('showcase')
 
-    let figure = document.createElement('figure')
+    // let figure = document.createElement('figure')
     let img = document.createElement('img')
     img.src = src;
     img.classList.add('photo')
-    figure.appendChild(img)
-    showcase.appendChild(figure)
+    // figure.appendChild(img)
+    showcase.appendChild(img)
   }
 }
 
