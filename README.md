5376892168704e9a9d6d9a107d62bba198b9ab58
Jared Radin <jared.radin@gmail.com>
2015-12-19 19:44:11 -0800

    layout and first js

---
+++ README.md
@@ -0,0 +1,7 @@
+# a portfolio of images served out of an S3 bucket
+
+`npm install -g webpack webpack-dev-server http-server`
+
+`npm install`
+
+`npm start`
--- js/index.js
+++ js/index.js
@@ -4,3 +4,22 @@
 require('../scss/index.scss');
 
 console.log('this is javascript')
+
+function render() {
+  let src = 'https://placekitten.com/1000/700'
+
+  let i = 20;
+
+  while (i--) {
+    let showcase = document.getElementById('showcase')
+
+    let figure = document.createElement('figure')
+    let img = document.createElement('img')
+    img.src = src;
+    img.classList.add('photo')
+    figure.appendChild(img)
+    showcase.appendChild(figure)
+  }
+}
+
+render()
